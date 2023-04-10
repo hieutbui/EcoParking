755f8d375abab66c506b3cc28c6c8769f9abebd3
hieubt <80142234+hieutbui@users.noreply.github.com>
2023-04-10 22:08:18 +0700

    re-config  eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,24 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'detox', 'jest', 'flowtype'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    '@react-native-community',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'detox', 'jest', 'flowtype'],
   rules: {
     '@typescript-eslint/no-shadow': ['error'],
     'no-shadow': 'off',
     'no-undef': 'off',
     'react-native/no-inline-styles': 'off',
     radix: 'off',
-  },
-  env: {
-    'detox/detox': true,
   },
 };
