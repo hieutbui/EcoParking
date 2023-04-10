07d04a4e6e8a31d05d98d9e6ddaf74e852315aba
hieubt <80142234+hieutbui@users.noreply.github.com>
2023-04-10 17:30:58 +0700

    test husky

--- app/screens/home/HomeScreen.jsx
+++ app/screens/home/HomeScreen.jsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 
 export const HomeScreen = () => {
   let test;
-  test = test1.data;
+  test = test1.data.helo;
   return (
     <View>
       <Text>Home</Text>
