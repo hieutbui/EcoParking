8927c0303ffbc0d809e20b5fe37ca1ee29903c97
hieubt <80142234+hieutbui@users.noreply.github.com>
2023-04-10 10:38:12 +0700

    test husky

--- app/screens/home/HomeScreen.jsx
+++ app/screens/home/HomeScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 export const HomeScreen = () => {
-    use
+  useEffect(() => {});
   return (
     <View>
       <Text>Home</Text>
