9dbdca664c5f98d5a339319ad9b7abceb18b397b
hieubt <80142234+hieutbui@users.noreply.github.com>
2023-04-11 23:19:53 +0700

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,12 @@ module.exports = {
     'no-duplicate-imports': ['error', { includeExports: false }],
     '@typescript-eslint/consistent-type-imports': 'off',
     'spaced-comment': ['warn', 'never'],
+    'prettier/prettier': [
+      'warn',
+      {
+        jsxSingleQuote: false,
+        parser: 'flow',
+      },
+    ],
   },
 };
