63a988920784674ac87aaff2ef645105de17d753
hieubt <trunghieu.bui201@gmail.com>
2023-07-06 00:15:53 +0700

    update

--- app/screens/booking/BookingScreen.jsx
+++ app/screens/booking/BookingScreen.jsx
@@ -1,4 +1,5 @@
 import Assets from 'app/assets/Assets';
+import { Font, FontSize } from 'app/constants/Styles';
 import api from 'app/controllers/api';
 import { Header } from 'app/shared/components/Header';
 import { RadiusButton } from 'app/shared/components/RadiusButton';
@@ -19,19 +20,15 @@ export const BookingScreen = () => {
           alignItems: 'center',
         }}
       >
-        <RadiusButton
-          title="get route"
-          type="positive"
-          onPress={async () => {
-            const route = await api.park.getDirection({
-              startLongitude: 105.847556,
-              startLatitude: 20.976937,
-              endLongitude: 20.976937,
-              endLatitude: 21.030765,
-            });
-            console.log({ direction: route?.routes[0]?.geometry });
+        <Text
+          style={{
+            fontFamily: Font.semiBold,
+            fontSize: FontSize.s_20,
+            color: Assets.AppColors.feature,
           }}
-        />
+        >
+          Coming Soon!
+        </Text>
       </View>
     </View>
   );
--- app/screens/saved/SavedScreen.jsx
+++ app/screens/saved/SavedScreen.jsx
@@ -1,4 +1,5 @@
 import Assets from 'app/assets/Assets';
+import { Font, FontSize } from 'app/constants/Styles';
 import { Header } from 'app/shared/components/Header';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
@@ -13,7 +14,24 @@ export const SavedScreen = () => {
       }}
     >
       <Header title={t('MyBookmark')} />
-      <View style={{ backgroundColor: Assets.AppColors.white, flex: 1 }} />
+      <View
+        style={{
+          backgroundColor: Assets.AppColors.white,
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: Font.semiBold,
+            color: Assets.AppColors.feature,
+            fontSize: FontSize.s_20,
+          }}
+        >
+          Coming Soon!
+        </Text>
+      </View>
     </View>
   );
 };
